9b4371bb162acff2844613ced28891035c34ea12
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2014-02-23 00:27:11 +0900

    Move some file to sub-directory, and write LICENSE.

---
+++ README.md
@@ -0,0 +1,35 @@
+MySQL Console
+=============
+
+This software provides you "MySQL Console". This MySQL Console talks the MySQL Client/Server protocol, therefore this application can connect to MySQL server directly, and you don't need to install other applications.
+
+Copyright
+---------
+
+Copyright (c) 2014 Yoichiro Tanaka. All rights reserved.
+
+LICENSE
+-------
+
+[Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Dependency
+----------
+
+This software includes the work that is distributed in the Apache License 2.0
+* [stringencoding](https://code.google.com/p/stringencoding/)
+
+This software includes the work that is distributed in the New BSD License
+* [CryptoJS](https://code.google.com/p/crypto-js/)
--- console.js
+++ console.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014 Yoichiro Tanaka. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var Console = function() {
     this.constructor();
 };
@@ -82,6 +98,7 @@ Console.prototype = {
         this.output(" ", false);
         this.output(manifest.name + " " + manifest.version, false);
         this.output("Copyright (c) Yoichiro Tanaka 2014. All rights reserved.", false);
+        this.output("Apache License 2.0(http://www.apache.org/licenses/LICENSE-2.0)", false);
         this.output(" ", false);
         this.output("If you want to know how to use, type the following command:", false);
         this.output("> help", false);
@@ -89,7 +106,9 @@ Console.prototype = {
         this.output("This software includes the work that is distributed in the Apache License 2.0", false);
         this.output("  stringencoding(https://code.google.com/p/stringencoding/)", false);
         this.output("This software includes the work that is distributed in the New BSD License", false);
-        this.output("  CryptoJS(https://code.google.com/p/crypto-js/)", true);
+        this.output("  CryptoJS(https://code.google.com/p/crypto-js/)", false);
+        this.output("This software includes the work that is distributed in the MIT License", false);
+        this.output("  jQuery(http://jquery.com/)", true);
     },
     quit: function() {
         window.close();
